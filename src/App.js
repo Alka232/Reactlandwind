a1d1e468ad80443a21f53addb7d0c3964a41408e
Alka Chauhan <alkachauhan492@gmail.com>
2024-06-14 23:27:08 +0530

    all sections are added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import Footer from './components/Footer';
 import { Fragment } from 'react';
-import Hero from'./components/Hero'
-import User from './components/User'
-import Products from './components/Products'
+import Hero from'./components/Hero';
+import User from './components/User';
+import Products from './components/Products';
+import Clients from './components/Clients';
+import Usedtool from './components/Usedtool';
+import InvestPotential from './components/InvestPotential';
+import TrustedWorld from './components/TrustedWorld';
+import Michael from './components/Michael';
+import BussinesTeam from './components/BussinesTeam';
+import Trail from './components/Trail';
+
 
 function App() {
   return (
@@ -13,10 +21,17 @@ function App() {
       <Header />
       <main className="bg-white">
       <Hero/>
+      <Clients/>
+      <Usedtool/>
+      <Products/>
+      <InvestPotential/>
+      <TrustedWorld/>
+      <Michael/>
+      {/* <BussinesTeam/> */}
+      <Trail/>
       </main>
 
       <User/>
-      <Products/>
 
       <Footer />
     </>
